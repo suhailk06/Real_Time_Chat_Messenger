0f7b09271d24dedf0cf2c603f09a5ad21450f76e
suhailk06 <suhailkanamukkala@gmail.com>
2026-08-15 17:06:27 +0530

    updated settings

--- dot_chat/dot_chat/wsgi.py
+++ dot_chat/dot_chat/wsgi.py
@@ -1,13 +1,16 @@
 """
 WSGI config for dot_chat project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dot_chat.settings")
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "dot_chat.settings"
+)
+
 
 application = get_wsgi_application()
